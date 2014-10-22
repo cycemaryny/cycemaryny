66c3a6e638aab0d618054f9de9b8f03e7e5ddf80
cycemaryny <b1044018@trbvm.com>
2014-10-22 23:07:11 +0200

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("CYCEMARYNY");
+            Console.WriteLine("CYCEMARYNY2");
             Console.ReadKey();
         }
     }
